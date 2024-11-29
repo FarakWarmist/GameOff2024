b1723d23d0c9d141a1636698edf9a57b1ee0e5eb
romanleco <94546246+romanleco@users.noreply.github.com>
2024-11-29 11:37:03 -0300

    Display the best time on the main menu
    The record time now shows up on the main menu

--- Assets/Scripts/UI/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -2,15 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using TimeFormatterUtil;
 
 public class MainMenuButtons : MonoBehaviour
 {
     [SerializeField] private Button _playButton; 
     [SerializeField] private Button _settingsButton; 
     [SerializeField] private Button _quitButton;
+    [SerializeField] private TMP_Text _recordTimeText;
 
     void Start()
     {
+        DataContainer loadedData = SaveManager.Instance.Load();
+        if(loadedData != null)
+        {
+            if(loadedData.bestTime == 0)
+            {
+                _recordTimeText.text = "";
+            }
+            else
+            {
+                _recordTimeText.text = "Record Time: " + TimeFormatter.GetTextFromTime(loadedData.bestTime);
+            }
+        }
+
         if(_playButton == null) Debug.LogError("Play Button is NULL");
         if(_settingsButton == null) Debug.LogError("Settings Button is NULL");
         if(_quitButton == null) Debug.LogError("Quit Button is NULL");
