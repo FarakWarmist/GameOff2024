35190d8ae3199816f99a1fa9168e054174d85c1e
romanleco <94546246+romanleco@users.noreply.github.com>
2024-12-02 12:18:29 -0300

    After Game Jam Update - Text for the values on the sliders
    Added texts to display the values of the sliders represented with floats

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class SettingsMenu : MonoBehaviour
 {
     [SerializeField] private Slider _musicSlider;
     [SerializeField] private Slider _soundEffectsSlider;
     [SerializeField] private Slider _mouseSensitivitySlider;
+    [SerializeField] private TMP_Text _musicValueText;
+    [SerializeField] private TMP_Text _soundEffectsValueText;
+    [SerializeField] private TMP_Text _mouseSensitivityValueText;
     [SerializeField] private Button _restartButton;
     [SerializeField] private Button _mainMenuButton;
     [SerializeField] private Button _quitButton;
@@ -28,6 +33,10 @@ public class SettingsMenu : MonoBehaviour
             _musicSlider.value = loadedData.musicVolume;
             _soundEffectsSlider.value = loadedData.sEffectsVolume;
             _mouseSensitivitySlider.value = loadedData.mouseSensitivity;
+
+            _musicValueText.text = "0." + Math.Truncate(loadedData.musicVolume * 100).ToString();
+            _soundEffectsValueText.text = "0." + Math.Truncate(loadedData.sEffectsVolume * 100).ToString();
+            _mouseSensitivityValueText.text = "0." + Math.Truncate(loadedData.mouseSensitivity * 100).ToString();
         }
     }
 
@@ -35,17 +44,20 @@ public class SettingsMenu : MonoBehaviour
     {
         AudioManager.Instance.SetVolume("Music", _musicSlider.value);
         SaveManager.Instance.Save(_musicSlider.value, "MUSIC");
+        _musicValueText.text = "0." + Math.Truncate(_musicSlider.value * 100).ToString();
     }
 
     public void ChangeSoundEffectsVolume()
     {
         AudioManager.Instance.SetVolume("Effects", _soundEffectsSlider.value);
         SaveManager.Instance.Save(_soundEffectsSlider.value, "EFFECTS");
+        _soundEffectsValueText.text = "0." + Math.Truncate(_soundEffectsSlider.value * 100).ToString();
     }
 
     public void ChangeMouseSensitivity()
     {
         SaveManager.Instance.SaveMouseSensitivity(_mouseSensitivitySlider.value);
+        _mouseSensitivityValueText.text = "0." + Math.Truncate(_mouseSensitivitySlider.value * 100).ToString();
     }
 
     private void Restart()
